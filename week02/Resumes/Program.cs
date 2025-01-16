8edf4e188d28ec5af41cb8076949524096eed1dc
JoseScoto <esc16004@byui.edu>
2025-01-15 23:24:45 -0600

    Resume program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        // Creating a job object or instance
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Adding instance of the Resume class
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        
+        // Adding the jobs to the list of the resume
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
