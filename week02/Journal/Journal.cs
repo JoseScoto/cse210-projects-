e13549928ff136a852c18b1e637d99659a8955ab
JoseScoto <esc16004@byui.edu>
2025-01-16 21:11:08 -0600

    Stub classes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Journal
+{
+   // Member variables
+   public List<Entry> _entries;
+
+    // Methods 
+   public void AddEntry(Entry newEntry)
+   {
+
+   }
+
+   public void DisplayAll()
+   {
+
+   }
+
+   public void SaveToFile(string file)
+   {
+
+   }
+
+   public void LoadFromFile(string file)
+   {
+
+   }
+
+}
+
